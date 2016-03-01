4e78514153fcc7078988cf0e798316d7b436ca0d
Starry HO <kmhoab@ust.hk>
2016-03-01 22:59:18 +0800

    test

--- k_sweep.sh
+++ k_sweep.sh
@@ -20,6 +20,7 @@ for qsize in 200; do
     rm -rf dctcpbb-q$qsize
     mkdir dctcpbb-q$qsize
     dir1=dctcpbb-q$qsize
+    #for k in 3 5 9 15 20 30 40 60 80 100; do
     for k in 3 5 10 20 30 35 40; do
         dctcp_red_min=`expr $k \\* $dctcp_red_avpkt`
         dctcp_red_max=`expr $dctcp_red_min + 1`
