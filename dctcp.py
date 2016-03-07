6941d890705393fbf79353210d1ca0d2b3963e76
starry9457 <kmhoab@ust.hk>
2016-03-08 05:35:23 +0800

    dctcp.py Trial 5!

--- dctcp.py
+++ dctcp.py
@@ -334,7 +334,7 @@ def dctcp():
 
     #if args.tcpdump:
     for i in xrange(args.hosts):
-        node_name = 'h%d' % (i+1)
+        node_name = 'h%d' % (i)
         net.getNodeByName(node_name).popen('/bin/ping 10.0.0.1 > %s/ping.txt' % args.dir, shell=True)
         #net.getNodeByName('h3').popen('/bin/ping 10.0.0.1 > %s/ping.txt' % args.dir, shell=True)
 
