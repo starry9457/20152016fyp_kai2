5257793b2ebe4d5c878da67cc2bc823e4a2cf758
Starry Ho <starry9457@gmail.com>
2016-03-12 23:14:44 +0800

    May be better solution to ping tests.
    - Use external .sh file to generate ping-avg.txt
    - Use '-' instead of '_' to solve the problem of shell script variables
    error.

---
+++ dctcp_pingavg.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Parameters
+qsize=200
+ks="3 5 8 15 20 30 40 60 80 100"
+n=3
+iperf_port=5001
+for ((i=0; i<$n; i++)); do
+	echo "Host: $i"
+	for k in $ks; do
+		dir1=dctcpbb-q$qsize-k$k
+		dctcp_src=$dir1/k$k-h$i-ping.txt
+		echo $k, |tr "\n" " " 
+		pingavg=`awk -F '/' 'END {print $5}' "$dctcp_src"`
+		echo $pingavg
+    done
+	echo ""
+done
---
+++ post_processing.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#echo "------------------------------------------------------------------------"
+#echo "post-processing.sh: This script is doing some post-experiment task."
+#echo "------------------------------------------------------------------------"
+
+# Parameters
+qsize=200
+ks="3 5 8 15 20 30 40 60 80 100"
+n=3
+iperf_port=5001
+
+dctcpf=dctcpgraph-q$qsize
+tcpecnf=tcpecngraph-q$qsize
+
+echo ""
+echo "------------------------------------------------------------------------"
+echo "Processing data to get average ping in different tests, result will be"
+echo "put inside $dctcpf and $tcpecnf"
+echo "------------------------------------------------------------------------"
+./tcpecn_pingavg.sh > $tcpecnf/ping-avg.txt
+./dctcp_pingavg.sh > $dctcpf/ping-avg.txt
+
+echo ""
+echo "------------------------------------------------------------------------"
+echo "Generating the graph of cwnd size comparison between DCTCP and TCP/ECN"
+echo "------------------------------------------------------------------------"
+for k in $ks; do
+    # cwnd graph. Actually not used in this experiment.
+    python plot_tcpprobe.py -f $dir1/cwnd.txt $dir2/cwnd.txt -o $dctcpf/cwnd-iperf-k$k.png -p $iperf_port
+done
