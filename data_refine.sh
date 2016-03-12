bc6a4805c5842f775fb4cb9eda12906e58bcbef4
None <None>
2016-03-12 06:28:53 +0000

    modified:   data_refine.sh

--- data_refine.sh
+++ data_refine.sh
@@ -18,21 +18,20 @@ echo "Finding the average ping in each test, combine them in to one file."
 echo "------------------------------------------------------------------------"
 
 for k in $ks; do
-	dir1=dctcpbb-q$qsize-k$k
-	dir2=tcpecnbb-q$qsize-k$k
-	for ((i=0; i<$n; i++)); do
-		echo "data_refine.sh: Combining with k: $k, h: $i"
-		#echo "kai2_expt_dctcp.sh: Combining with k: $k, h: $i"
-		
-		echo $k, |tr "\n" " " >> $dctcpf/k$k_ping.txt
-		awk -F '/' 'END {print $5}' dir1/k$k_h$i_ping.txt >> $dctcpf/k$k_h$i_ping_avg.txt
-		
-		echo $k, |tr "\n" " " >> $tcpecnf/k$k_ping.txt
-		awk -F '/' 'END {print $5}' dir1/k$k_h$i_ping.txt >> $tcpecnf/k$k_h$i_ping_avg.txt
-	done
-	
-	# cwnd graph, not used.
-	python plot_tcpprobe.py -f $dir1/cwnd.txt $dir2/cwnd.txt -o $dctcpf/cwnd-iperf-k$k.png -p $iperf_port
+    dir1=dctcpbb-q$qsize-k$k
+    dir2=tcpecnbb-q$qsize-k$k
+    for ((i=0; i<$n; i++)); do
+        echo "data_refine.sh: Combining with k: $k, h: $i"
+        #echo "kai2_expt_dctcp.sh: Combining with k: $k, h: $i"
+        echo $k, |tr "\n" " " >> $dctcpf/k$k_ping.txt
+        awk -F '/' 'END {print $5}' dir1/k$k_h$i_ping.txt >> $dctcpf/k$k_h$i_ping_avg.txt
+	    
+        echo $k, |tr "\n" " " >> $tcpecnf/k$k_ping.txt
+        awk -F '/' 'END {print $5}' dir1/k$k_h$i_ping.txt >> $tcpecnf/k$k_h$i_ping_avg.txt
+    done
+
+    # cwnd graph, not used.
+    python plot_tcpprobe.py -f $dir1/cwnd.txt $dir2/cwnd.txt -o $dctcpf/cwnd-iperf-k$k.png -p $iperf_port
 done
 
 
