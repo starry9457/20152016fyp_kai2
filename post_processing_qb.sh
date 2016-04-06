06cb12ecf4e8d7230624ac024a3278ac0a61d45d
starry9457 <kmhoab@ust.hk>
2016-04-06 09:33:06 +0800

    fix problem for wrong path

--- post_processing_qb.sh
+++ post_processing_qb.sh
@@ -10,8 +10,8 @@ ks="3 5 8 15 20 30 40 60 80 100"
 n=3
 iperf_port=5001
 
-dctcpf=qbdctcpgraphs-q$qsize
-tcpecnf=qbtcpecngraphs-q$qsize
+dctcpf=qb-dctcpgraphs-q$qsize
+tcpecnf=qb-tcpecngraphs-q$qsize
 
 echo ""
 echo "------------------------------------------------------------------------"
@@ -27,8 +27,8 @@ echo "Generating the graph of cwnd size comparison between DCTCP and TCP/ECN"
 echo "------------------------------------------------------------------------"
 for k in $ks; do
     # cwnd graph. Actually not used in this experiment.
-    dir1=qbdctcpbb-q$qsize-k$k
-    dir2=qbtcpecnbb-q$qsize-k$k
+    dir1=qb-dctcpbb-q$qsize-k$k
+    dir2=qb-tcpecnbb-q$qsize-k$k
     python plot_tcpprobe.py -f $dir1/cwnd.txt $dir2/cwnd.txt -o $dctcpf/cwnd-iperf-k$k.png -p $iperf_port
 done
 echo ""
@@ -37,8 +37,8 @@ echo "Generating the graph of queue occupancy comparison between DCTCP and TCP/E
 echo "------------------------------------------------------------------------"
 for k in $ks; do
     # cwnd graph. Actually not used in this experiment.
-    dir1=qbdctcpbb-q$qsize-k$k
-    dir2=qbtcpecnbb-q$qsize-k$k
+    dir1=qb-dctcpbb-q$qsize-k$k
+    dir2=qb-tcpecnbb-q$qsize-k$k
     python plot_queue.py -f $dir1/q.txt $dir2/q.txt --legend dctcp tcpecn -o $dctcpf/dctcp-tcpecn-queue-k$k.png
 done
 
